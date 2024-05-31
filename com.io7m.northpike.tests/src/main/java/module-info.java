d3a17303490ab6af319235648970e8cd1444ac91
Mark Raynsford <code@io7m.com>
2024-05-31 16:16:00 +0000

    Use junit BOM.

--- com.io7m.northpike.tests/src/main/java/module-info.java
+++ com.io7m.northpike.tests/src/main/java/module-info.java
@@ -162,8 +162,9 @@ open module com.io7m.northpike.tests
   requires org.slf4j;
   requires org.xerial.sqlitejdbc;
 
-  requires transitive org.junit.jupiter.api;
-  requires transitive org.junit.jupiter.engine;
-  requires transitive org.junit.platform.commons;
-  requires transitive org.junit.platform.engine;
+  requires org.junit.jupiter.api;
+  requires org.junit.jupiter.engine;
+  requires org.junit.platform.commons;
+  requires org.junit.platform.engine;
+  requires org.junit.platform.launcher;
 }
